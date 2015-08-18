0a908500982a69aa3b135243475595919bcffb8c
Michael Gubbels <mrgubbels@gmail.com>
2015-08-18 02:10:12 -0700

    Added basic interactive "REPL" pattern.

--- Firmware/R5/Sources/main.c
+++ Firmware/R5/Sources/main.c
@@ -40,14 +40,16 @@
 
 /* User includes (#include below this line is not maintained by Processor Expert) */
 
-int value;
-char buffer[64];
-
 /*lint -save  -e970 Disable MISRA rule (6.3) checking. */
 int main(void)
 /*lint -restore Enable MISRA rule (6.3) checking. */
 {
   /* Write your local variable definition here */
+  int value;
+  int i;
+  int flag;
+  char buffer[64] = { 0 };
+  int ch = (int) 'a';
 
   /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
   PE_low_level_init();
@@ -57,6 +59,9 @@ int main(void)
   /* For example: for(;;) { } */
   
   for (;;) {
+	  printf ("Clay\r\n");
+	  
+	  /*
 	  printf ("What's your name?\r\n");
 	  printf ("> ");
 	  scanf  ("%s", buffer);
@@ -66,17 +71,55 @@ int main(void)
 	  printf ("> ");
 	  scanf  ("%i", &value);
 	  printf ("I'm on it. You'll soon have %i bits of Clay.\r\n", value);
+	  */
+	  
+//	  printf ("Continue? (y/n)\r\n");
+//	  while (buffer[0] != 'n') {
+//		  fgets (buffer, sizeof (buffer), stdin);
+//		  printf ("%s", buffer);
+//	  }
+	  
+	  /*
+	  // Simple real-time I/O
+	  do {
+		  ch = getchar (); // ch = getc (stdin);
+		  putchar (ch); // printf ("%c", ch);
+		  fflush (stdin);
+		  fflush (stdout);
+	  } while (ch != '.'); // Read characters until the '.' character is entered. This could be replaced with '\n' or another character, or a different condition.
+	  */
+	  
+	  /*
+	  // Read a line into a buffer (until "\r\n" is received)
+	  i = 0;
+	  do {
+		  buffer[i] = getchar ();
+		  putchar (buffer[i]);
+		  if (buffer[i] == '\r') { printf ("<return>"); }
+		  if (buffer[i] == '\n') { printf ("<newline>"); }
+		  i++;
+		  fflush (stdin);
+		  fflush (stdout);
+	  } while (i > 1 ? !(buffer[i - 1] == '\r' && buffer[i] == '\n') : TRUE); // Read characters until the '.' character is entered. This could be replaced with '\n' or another character, or a different condition.
+	  */
+	  
+	  // "REPL" framework
+	  for (;;) {
+		  i = 0;
+		  flag = 0;
+		  ch = (int) 0;
+		  printf (">");
+		  do {
+			  ch = getchar (); // ch = getc (stdin);
+			  buffer[i++] = ch; // Append the character to the buffer.
+			  putchar (ch); // Echo the character.
+			  if (ch == '\n') { /* printf ("<newline>"); */ flag = 1; } // Show when a newline character is entered.
+		  } while (flag == 0);
+		  buffer[i] = '\0'; // Terminate the string
+		  printf ("%s", buffer);
+	  }
+	  
   }
-  
-//    char ch;
-//                  
-//    printf("\nHello World!!\n");
-//
-//  	while(1)
-//  	{
-//  		ch = in_char();
-//  		out_char(ch);
-//  	} 
 
   /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
   /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
