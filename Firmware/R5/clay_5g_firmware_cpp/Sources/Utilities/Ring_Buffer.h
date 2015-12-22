cd4365523eff59d6e4d3119253ce018596ea6247
Ben Hefner <thebhef@gmail.com>
2015-12-21 23:48:34 -0600

    added cpp version of firmware project.

---
+++ Firmware/R5/clay_5g_firmware_cpp/Sources/Utilities/Ring_Buffer.h
@@ -0,0 +1,104 @@
+/*
+ * Ring_Buffer.h
+ *
+ *  Created on: Sep 16, 2015
+ *      Author: mokogobo
+ */
+
+#ifndef RING_BUFFER_H_
+#define RING_BUFFER_H_
+
+/* Include shared modules, which are used for whole project */
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+/* Include inherited beans */
+
+#include "Cpu.h"
+
+
+#define Ring_Buffer_BUF_SIZE   2048  /* number of elements in the buffer */
+
+
+byte Ring_Buffer_Put (byte elem);
+/*
+** ===================================================================
+**     Method      :  Ring_Buffer_Put (component RingBufferUInt8)
+**     Description :
+**         Puts a new element into the buffer
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**         elem            - New element to be put into the buffer
+**     Returns     :
+**         ---             - Error code
+** ===================================================================
+*/
+
+byte Ring_Buffer_Get (byte *elemP);
+/*
+** ===================================================================
+**     Method      :  Ring_Buffer_Get (component RingBufferUInt8)
+**     Description :
+**         Removes an element from the buffer
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**       * elemP           - Pointer to where to store the received
+**                           element
+**     Returns     :
+**         ---             - Error code
+** ===================================================================
+*/
+
+uint8_t Ring_Buffer_Has_Data ();
+/*
+** ===================================================================
+**     Method      :  Ring_Buffer_Has_Data (component RingBufferUInt8)
+**     Description :
+**         Checks if the buffer contains any data.
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**       * elemP           - Pointer to where to store the received
+**                           element
+**     Returns     :
+**         ---             - Error code
+** ===================================================================
+*/
+
+uint16_t Ring_Buffer_NofElements (void);
+/*
+** ===================================================================
+**     Method      :  Ring_Buffer_NofElements (component RingBufferUInt8)
+**     Description :
+**         Returns the actual number of elements in the buffer.
+**     Parameters  : None
+**     Returns     :
+**         ---             - Number of elements in the buffer.
+** ===================================================================
+*/
+
+void Ring_Buffer_Init (void);
+/*
+** ===================================================================
+**     Method      :  Ring_Buffer_Init (component RingBufferUInt8)
+**     Description :
+**         Initializes the data structure
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+uint16_t Ring_Buffer_NofFreeElements (void);
+/*
+** ===================================================================
+**     Method      :  Ring_Buffer_NofFreeElements (component RingBufferUInt8)
+**     Description :
+**         Returns the actual number of free elements/space in the
+**         buffer.
+**     Parameters  : None
+**     Returns     :
+**         ---             - Number of elements in the buffer.
+** ===================================================================
+*/
+
+#endif /* RING_BUFFER_H_ */
