10619b0c3fa10df441cfdf451c86c08353247e1c
Ben Hefner <thebhef@gmail.com>
2016-02-07 00:58:27 -0800

    Updated references to be relative to main orcad project. Added my capture.ini.

--- Firmware/R6/C6_Bringup/Generated_Code/CPU_Config.h
+++ Firmware/R6/C6_Bringup/Generated_Code/CPU_Config.h
@@ -6,7 +6,7 @@
 **     Version     : Component 01.046, Driver 01.00, CPU db: 3.00.000
 **     Repository  : Kinetis
 **     Compiler    : GNU C Compiler
-**     Date/Time   : 2016-02-06, 19:20, # CodeGen: 43
+**     Date/Time   : 2016-02-06, 20:59, # CodeGen: 46
 **
 **     Copyright : 1997 - 2015 Freescale Semiconductor, Inc. 
 **     All Rights Reserved.
--- Firmware/R6/C6_Bringup/Generated_Code/WIFI_GPIO2.h
+++ Firmware/R6/C6_Bringup/Generated_Code/WIFI_GPIO2.h
@@ -7,7 +7,7 @@
 **     Version     : Component 01.033, Driver 01.03, CPU db: 3.00.000
 **     Repository  : Kinetis
 **     Compiler    : GNU C Compiler
-**     Date/Time   : 2016-01-30, 15:14, # CodeGen: 19
+**     Date/Time   : 2016-02-06, 20:55, # CodeGen: 45
 **     Abstract    :
 **         The HAL BitIO component provides a low level API for unified
 **         access to general purpose digital input/output pins across
