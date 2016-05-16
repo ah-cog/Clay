a6bdd4380445156043990dbdf16636b4cf128a4f
Ben Hefner <thebhef@gmail.com>
2016-05-16 01:02:39 -0700

    interactive assembly checkpoint
    implemented multiple channels. seems to work OK when the messages get
    through.
    currently can't remove channels or change direction after a channel has
    been established

--- Firmware/R6/Clay_C6_Firmware/Sources/Services/Interactive_Assembly.h
+++ Firmware/R6/Clay_C6_Firmware/Sources/Services/Interactive_Assembly.h
@@ -13,19 +13,19 @@
 ////Typedefs  /////////////////////////////////////////////////////
 typedef enum
 {
+   //IMPORTANT: we rely on pending state to be defined as the destination state + 1 (i.e: (input_pending = 1) => (input_destination = 0))
    INTERACTIVE_ASSEMBLY_NONE,                       //nothing happening; haven't received any button presses
-   INTERACTIVE_ASSEMBLY_LOCAL_INPUT_MASTER,         //received button press locally first
-   INTERACTIVE_ASSEMBLY_LOCAL_INPUT_SLAVE,     //user pushed the button on this module after receiving a message telling it to be input
+   INTERACTIVE_ASSEMBLY_LOCAL_INPUT_SOURCE,         //received button press locally first
+   INTERACTIVE_ASSEMBLY_LOCAL_INPUT_DESTINATION,     //user pushed the button on this module after receiving a message telling it to be input
    INTERACTIVE_ASSEMBLY_LOCAL_INPUT_PENDING,        //received message saying we should be config'd as input
-   INTERACTIVE_ASSEMBLY_LOCAL_OUTPUT_MASTER,        //received button press locally first
-   INTERACTIVE_ASSEMBLY_LOCAL_OUTPUT_SLAVE,     //user pushed the button on this module after receiving a message telling it to be output
+   INTERACTIVE_ASSEMBLY_LOCAL_OUTPUT_SOURCE,        //received button press locally first
+   INTERACTIVE_ASSEMBLY_LOCAL_OUTPUT_DESTINATION,     //user pushed the button on this module after receiving a message telling it to be output
    INTERACTIVE_ASSEMBLY_LOCAL_OUTPUT_PENDING,       //received message saying we should be config'd as output.
    INTERACTIVE_ASSEMBLY_CHANNEL_STATE_MAX
 } Interactive_Assembly_Channel_State;
 
 ////Globals   /////////////////////////////////////////////////////
-extern int8_t button_mode;     // 0 = default, 1 = select channel
-extern uint32_t button_mode_timeout;
+extern int8_t interactive_assembly_using_lights;
 
 ////Function Prototypes ///////////////////////////////////////////
 
